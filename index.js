1ef388ff2b2b607352c4351a348245f5ffb984c7
Your Namevickase.a@gmail.com <vickase.a@gmail.com>
2018-06-03 22:51:11 -0300

    Added format stuff

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var youtubedl = require('youtube-dl');
+var ffmpeg = require('fluent-ffmpeg');
+var argv = require('minimist')(process.argv.slice(2));
 
+// miPrograma -i [URL] --format=[audio|video]
+// Ejemple: node . -i https://www.youtube.com/watch?v=Pv0PAchVGCw --format video
 
 const DOWNLOAD_DIR = path.join(__dirname, 'downloads');
-const URL_YOUTUBE = process.argv[2];
+const URL_YOUTUBE = argv.i;
 
-if (!URL_YOUTUBE) {
-  throw Error("HACE FALTA URL!!");
+if (!argv.i || (argv.format!=='audio' && argv.format!=='video')) {
+  throw Error("Usage: miPrograma -i [URL] --format=[audio|video]");
   return;
 }
 
@@ -34,6 +38,20 @@ mkdirp(DOWNLOAD_DIR, function(err) {
     
     video.on('end', function() {
       console.log('finished downloading!');
+
+      if(argv.format === "audio") {
+        console.log("conversion starting...");
+        const videoInput = path.join(DOWNLOAD_DIR, filename);
+        const tempOutput = path.join(DOWNLOAD_DIR, filename).split(".");
+        tempOutput.splice(-1);
+        const audioOutput = tempOutput + ".mp3";
+        
+        convert(videoInput, audioOutput, function(err){
+          if(err) throw err;
+          console.log('conversion complete');
+          fs.unlinkSync(videoInput);
+        });
+      }
     });
   
     video.pipe(fs.createWriteStream(path.join(DOWNLOAD_DIR, filename)));
@@ -41,3 +59,14 @@ mkdirp(DOWNLOAD_DIR, function(err) {
 });
 
 
+function convert(input, output, callback) {
+  ffmpeg(input)
+      .output(output)
+      .on('end', function() {                    
+          console.log('conversion ended');
+          callback(null);
+      }).on('error', function(err){
+          console.log('error: ', e.code, e.msg);
+          callback(err);
+      }).run();
+}
